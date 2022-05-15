2af4cf6dab98c83a5d5f24ad4e21fbca7a007763
aiyu666 <zrobin8741@gmail.com>
2022-05-15 22:44:26 +0800

    Update the candlestick

--- challenges_API/tests/rest_API/test_get_candlestick.py
+++ challenges_API/tests/rest_API/test_get_candlestick.py
@@ -5,17 +5,25 @@ from utils.common_api.candlestick_steps import (
     verify_candlestick_data_is_correct,
     verify_candlestick_data_is_correct_when_timeframe_is_wrong,
     verify_candlestick_data_is_correct_when_instrument_name_is_wrong,
-    verify_candlestick_data_is_correct_when_params_without_required_parameter
+    verify_candlestick_data_is_correct_when_params_without_required_parameter,
 )
 
 
 @pytest.mark.parametrize(
     "instrument_name, timeframe",
     [
-        ("BTC_USDT", "1m"), ("ETH_USDT", "5m"), ("YFI_USDT", "15m"),
-        ("BTC_USDT", "30m"), ("ETH_USDT", "1h"), ("YFI_USDT", "4h"),
-        ("BTC_USDT", "6h"), ("ETH_USDT", "12h"), ("YFI_USDT", "1D"),
-        ("BTC_USDT", "7D"), ("ETH_USDT", "14D"), ("YFI_USDT", "1M")
+        ("BTC_USDT", "1m"),
+        ("ETH_USDT", "5m"),
+        ("YFI_USDT", "15m"),
+        ("BTC_USDT", "30m"),
+        ("ETH_USDT", "1h"),
+        ("YFI_USDT", "4h"),
+        ("BTC_USDT", "6h"),
+        ("ETH_USDT", "12h"),
+        ("YFI_USDT", "1D"),
+        ("BTC_USDT", "7D"),
+        ("ETH_USDT", "14D"),
+        ("YFI_USDT", "1M"),
     ],
 )
 @pytest.mark.candlestick
@@ -27,9 +35,15 @@ def test_user_can_get_candlestick(instrument_name, timeframe):
 @pytest.mark.parametrize(
     "instrument_name, timeframe",
     [
-        ("BTC_USDT", "0m"), ("ETH_USDT", "-1m",), ("YFI_USDT", "!"),
-        ("BTC_USDT", "99999999"), ("ETH_USDT", "15D",), ("YFI_USDT", "$!!@$@#%$"),
-        ("BTC_USDT", "-100M"), ("ETH_USDT", "-15D",), ("YFI_USDT", "."),
+        ("BTC_USDT", "0m"),
+        ("ETH_USDT", "-1m"),
+        ("YFI_USDT", "!"),
+        ("BTC_USDT", "99999999"),
+        ("ETH_USDT", "15D"),
+        ("YFI_USDT", "$!!@$@#%$"),
+        ("BTC_USDT", "-100M"),
+        ("ETH_USDT", "-15D"),
+        ("YFI_USDT", "."),
     ],
 )
 @pytest.mark.candlestick
@@ -37,11 +51,16 @@ def test_user_can_not_get_candlestick_with_wrong_timeframe(instrument_name, time
     response = user_can_not_get_candlestick_successfully(instrument_name, timeframe, "get_candlestick_failed_with_wrong_timeframe")
     verify_candlestick_data_is_correct_when_timeframe_is_wrong(response, timeframe)
 
+
 @pytest.mark.parametrize(
     "instrument_name, timeframe",
     [
-        ("~", "1m"), ("&", "5m"), ("*", "15m"),
-        ("@!#", "30m"), ("^", "1h"), ("!.", "4h"),
+        ("~", "1m"),
+        ("&", "5m"),
+        ("*", "15m"),
+        ("@!#", "30m"),
+        ("^", "1h"),
+        ("!.", "4h"),
     ],
 )
 @pytest.mark.candlestick
@@ -49,11 +68,16 @@ def test_user_can_not_get_candlestick_with_invalid_instrument_name(instrument_na
     response = user_can_not_get_candlestick_successfully(instrument_name, timeframe, "get_candlestick_failed_with_wrong_instrument_name")
     verify_candlestick_data_is_correct_when_instrument_name_is_wrong(response)
 
+
 @pytest.mark.parametrize(
     "instrument_name, timeframe",
     [
-        (None, "1m"), (None, "-5m"), (None, "!"),
-        ("TEST_TEST", None), ("^", None), ("USDT", None),
+        (None, "1m"),
+        (None, "-5m"),
+        (None, "!"),
+        ("TEST_TEST", None),
+        ("^", None),
+        ("USDT", None),
     ],
 )
 @pytest.mark.candlestick
